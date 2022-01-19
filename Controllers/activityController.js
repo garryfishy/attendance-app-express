49c977876f1a200ed5e0b2444a7d358ac0196962
Garry Agassi <garry.agassi@sprout.co.id>
2022-01-19 16:29:54 +0700

    add status and columns

--- Controllers/activityController.js
+++ Controllers/activityController.js
@@ -1,84 +1,99 @@
-const { kegiatan, clients, cases, user } = require("../models/");
+const { kegiatan, clients, cases, user, log } = require("../models/");
 const image = require("../helpers/imagekit");
 
 class activityController {
-	static async createActivity(req, res, next) {
-		try {
-			let {
-				kasus,
-				lawyer_id,
-				long,
-				lat,
-				subject,
-				client,
-				notes,
-				kegiatan_type_id,
-			} = req.body;
+  static async createActivity(req, res, next) {
+    try {
+      let {
+        kasus,
+        lawyer_id,
+        long,
+        lat,
+        subject,
+        client,
+        notes,
+        kegiatan_type_id,
+      } = req.body;
 
-			console.log(req.body);
+      console.log(req.body);
+      let status = "Pending";
 
-			const imageName = req.file.originalname;
-			const buffer = req.file.buffer.toString("base64");
-			let photo = await image(imageName, buffer);
-			photo = photo.url;
+      const imageName = req.file.originalname;
+      const buffer = req.file.buffer.toString("base64");
+      let photo = await image(imageName, buffer);
+      photo = photo.url;
 
-			let result = await kegiatan.create({
-				case: kasus,
-				photo,
-				long,
-				lat,
-				subject,
-				client,
-				notes,
-				kegiatan_type_id,
-				lawyer_id,
-			});
+      let result = await kegiatan.create({
+        case: kasus,
+        photo,
+        long,
+        lat,
+        subject,
+        status,
+        client,
+        notes,
+        kegiatan_type_id,
+        lawyer_id,
+      });
 
-			if (result) {
-				res.status(200).json(result);
-			}
-		} catch (error) {
-			res.status(500).json({ error: error.message });
-		}
-	}
+      if (result) {
+        await log.create({
+          name: kasus,
+          status,
+          long,
+          lat,
+          activity:
+            kegiatan_type_id === 1 ? "Start Kunjungan" : "Finish Kunjungan",
+          date: new Date(),
+          photo,
+          user_id: lawyer_id,
+          notes,
+        });
 
-	static async getAllKegiatanType(req, res, next) {
-		let kegiatanType = [
-			{
-				id: 1,
-				name: "Start Kunjungan",
-			},
-			{
-				id: 2,
-				name: "Finish Kunjungan",
-			},
-		];
-		res.status(200).json(kegiatanType);
-	}
+        res.status(200).json(result);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 
-	static async getAll(req, res, next) {
-		try {
-			let result = await kegiatan.findAll();
+  static async getAllKegiatanType(req, res, next) {
+    let kegiatanType = [
+      {
+        id: 1,
+        name: "Start Kunjungan",
+      },
+      {
+        id: 2,
+        name: "Finish Kunjungan",
+      },
+    ];
+    res.status(200).json(kegiatanType);
+  }
 
-			if (result) {
-				// let findClient = await client.findByPk(result.dataValues.client_id);
-				// let findCase = await cases.findByPk(result.dataValues.case_id);
-				// let findUser = await user.findByPk(result.dataValues.lawyer_id);
+  static async getAll(req, res, next) {
+    try {
+      let result = await kegiatan.findAll();
 
-				let emptyObj = [];
-				result.map((e) => {
-					e.dataValues.kegiatan_type_id = 1
-						? "Start Kunjungan"
-						: "Finish Kunjungan";
-					delete e.dataValues.client_id;
-					delete e.dataValues.case_id;
-					emptyObj.push(e.dataValues);
-				});
-				res.status(200).json(emptyObj);
-			}
-		} catch (error) {
-			res.status(500).json(error.message);
-		}
-	}
+      if (result) {
+        // let findClient = await client.findByPk(result.dataValues.client_id);
+        // let findCase = await cases.findByPk(result.dataValues.case_id);
+        // let findUser = await user.findByPk(result.dataValues.lawyer_id);
+
+        let emptyObj = [];
+        result.map((e) => {
+          e.dataValues.kegiatan_type_id = 1
+            ? "Start Kunjungan"
+            : "Finish Kunjungan";
+          delete e.dataValues.client_id;
+          delete e.dataValues.case_id;
+          emptyObj.push(e.dataValues);
+        });
+        res.status(200).json(emptyObj);
+      }
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
 }
 module.exports = activityController;
--- models/kegiatan.js
+++ models/kegiatan.js
@@ -1,34 +1,35 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class kegiatan extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-		}
-	}
-	kegiatan.init(
-		{
-			case: DataTypes.STRING,
-			photo: DataTypes.STRING,
-			subject: DataTypes.STRING,
-			client: DataTypes.STRING,
-			notes: DataTypes.STRING,
-			long: DataTypes.STRING,
-			lat: DataTypes.STRING,
-			kegiatan_type_id: DataTypes.INTEGER,
-			lawyer_id: DataTypes.INTEGER,
-			client_id: DataTypes.INTEGER,
-			case_id: DataTypes.INTEGER,
-		},
-		{
-			sequelize,
-			modelName: "kegiatan",
-		}
-	);
-	return kegiatan;
+  class kegiatan extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  kegiatan.init(
+    {
+      case: DataTypes.STRING,
+      photo: DataTypes.STRING,
+      subject: DataTypes.STRING,
+      client: DataTypes.STRING,
+      notes: DataTypes.STRING,
+      status: DataTypes.STRING,
+      long: DataTypes.STRING,
+      lat: DataTypes.STRING,
+      kegiatan_type_id: DataTypes.INTEGER,
+      lawyer_id: DataTypes.INTEGER,
+      client_id: DataTypes.INTEGER,
+      case_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "kegiatan",
+    }
+  );
+  return kegiatan;
 };
